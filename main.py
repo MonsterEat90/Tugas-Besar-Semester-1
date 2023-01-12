5e0208682df48c015890d1827d1df450e1557e92
MonsterEat90 <87372227+MonsterEat90@users.noreply.github.com>
2023-01-12 16:54:04 +0700

    import to excel successed

--- main.py
+++ main.py
@@ -1,68 +1,198 @@
-import tkinter as tk
+import sqlite3
+import pandas as pd
+from sqlalchemy import create_engine
+from tkinter import *
+from tkinter import ttk
 
-# Create the main window
-window = tk.Tk()
-window.title("CRUD App")
-
-# Create a list to store the items
-items = []
-
-# Create a function to add an item to the list
-
-
-def add_item():
-    # Get the item from the input field
-    item = entry.get()
-
-    # Add the item to the list
-    items.append(item)
-
-    # Clear the input field
-    entry.delete(0, tk.END)
-
-    # Update the list box with the new list
-    update_list_box()
-
-# Create a function to delete an item from the list
+root = Tk()
+root.title("Data Motor")
+root.geometry("1080x720")
+my_tree = ttk.Treeview(root)
+storeName = "Data Motor"
 
 
-def delete_item():
-    # Get the selected item from the list box
-    selected_item = list_box.get(list_box.curselection())
-
-    # Remove the item from the list
-    items.remove(selected_item)
-
-    # Update the list box with the new list
-    update_list_box()
-
-# Create a function to update the list box
+def reverse(tuples):
+    new_tup = tuples[::-1]
+    return new_tup
 
 
-def update_list_box():
-    # Clear the list box
-    list_box.delete(0, tk.END)
+def insert(id, name, price, quantity):
+    conn = sqlite3.connect("data.db")
+    cursor = conn.cursor()
 
-    # Insert each item in the list into the list box
-    for item in items:
-        list_box.insert(tk.END, item)
+    cursor.execute("""CREATE TABLE IF NOT EXISTS 
+    inventory(itemId TEXT, itemName TEXT, itemPrice TEXT, itemQuantity TEXT)""")
+
+    cursor.execute("INSERT INTO inventory VALUES ('" + str(id) + "','" +
+                   str(name) + "','" + str(price) + "','" + str(quantity) + "')")
+    conn.commit()
 
 
-# Create an input field and a button to add items to the list
-entry = tk.Entry(window)
-add_button = tk.Button(window, text="Add", command=add_item)
+def delete(data):
+    conn = sqlite3.connect("data.db")
+    cursor = conn.cursor()
 
-# Create a list box to display the items in the list
-list_box = tk.Listbox(window)
+    cursor.execute("""CREATE TABLE IF NOT EXISTS 
+        inventory(itemId TEXT, itemName TEXT, itemPrice TEXT, itemQuantity TEXT)""")
 
-# Create a button to delete items from the list
-delete_button = tk.Button(window, text="Delete", command=delete_item)
+    cursor.execute("DELETE FROM inventory WHERE itemId = '" + str(data) + "'")
+    conn.commit()
 
-# Add the widgets to the window
-entry.pack()
-add_button.pack()
-list_box.pack()
-delete_button.pack()
 
-# Run the main loop
-window.mainloop()
+def update(id, name, price, quantity,  idName):
+    conn = sqlite3.connect("data.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""CREATE TABLE IF NOT EXISTS 
+        inventory(itemId TEXT, itemName TEXT, itemPrice TEXT, itemQuantity TEXT)""")
+
+    cursor.execute("UPDATE inventory SET itemId = '" + str(id) + "', itemName = '" + str(name) +
+                   "', itemPrice = '" + str(price) + "', itemQuantity = '" + str(quantity) + "' WHERE itemId='"+str(idName)+"'")
+    conn.commit()
+
+
+def read():
+    conn = sqlite3.connect("data.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""CREATE TABLE IF NOT EXISTS 
+        inventory(itemId TEXT, itemName TEXT, itemPrice TEXT, itemQuantity TEXT)""")
+
+    cursor.execute("SELECT * FROM inventory")
+    results = cursor.fetchall()
+    conn.commit()
+    return results
+
+
+def insert_data():
+    itemId = str(entryId.get())
+    itemName = str(entryName.get())
+    itemPrice = str(entryPrice.get())
+    itemQuantity = str(entryQuantity.get())
+    if itemId == "" or itemName == " ":
+        print("Error Inserting Id")
+    if itemName == "" or itemName == " ":
+        print("Error Inserting Name")
+    if itemPrice == "" or itemPrice == " ":
+        print("Error Inserting Price")
+    if itemQuantity == "" or itemQuantity == " ":
+        print("Error Inserting Quantity")
+    else:
+        insert(str(itemId), str(itemName), str(itemPrice), str(itemQuantity))
+
+    for data in my_tree.get_children():
+        my_tree.delete(data)
+
+    for result in reverse(read()):
+        my_tree.insert(parent='', index='end', iid=result,
+                       text="", values=(result), tag="orow")
+
+    my_tree.tag_configure('orow', background='#EEEEEE')
+    my_tree.grid(row=1, column=5, columnspan=4, rowspan=5, padx=10, pady=10)
+
+
+def delete_data():
+    selected_item = my_tree.selection()[0]
+    deleteData = str(my_tree.item(selected_item)['values'][0])
+    delete(deleteData)
+
+    for data in my_tree.get_children():
+        my_tree.delete(data)
+
+    for result in reverse(read()):
+        my_tree.insert(parent='', index='end', iid=result,
+                       text="", values=(result), tag="orow")
+
+    my_tree.tag_configure('orow', background='#EEEEEE')
+    my_tree.grid(row=1, column=5, columnspan=4, rowspan=5, padx=10, pady=10)
+
+
+def update_data():
+    selected_item = my_tree.selection()[0]
+    update_name = my_tree.item(selected_item)['values'][0]
+    update(entryId.get(), entryName.get(), entryPrice.get(),
+           entryQuantity.get(), update_name)
+
+    for data in my_tree.get_children():
+        my_tree.delete(data)
+
+    for result in reverse(read()):
+        my_tree.insert(parent='', index='end', iid=result,
+                       text="", values=(result), tag="orow")
+
+    my_tree.tag_configure('orow', background='#EEEEEE')
+    my_tree.grid(row=1, column=5, columnspan=4, rowspan=5, padx=10, pady=10)
+
+
+def import_data():
+    engine = create_engine('sqlite:///data.db')
+    df = pd.read_sql_table("Inventory", engine)
+    df.to_excel("data.xlsx")
+
+
+titleLabel = Label(root, text=storeName, font=('Arial bold', 30), bd=2)
+titleLabel.grid(row=0, column=0, columnspan=8, padx=20, pady=20)
+
+idLabel = Label(root, text="ID", font=('Arial bold', 15))
+nameLabel = Label(root, text="Name", font=('Arial bold', 15))
+priceLabel = Label(root, text="Price", font=('Arial bold', 15))
+quantityLabel = Label(root, text="Quantity", font=('Arial bold', 15))
+idLabel.grid(row=1, column=0, padx=10, pady=10)
+nameLabel.grid(row=2, column=0, padx=10, pady=10)
+priceLabel.grid(row=3, column=0, padx=10, pady=10)
+quantityLabel.grid(row=4, column=0, padx=10, pady=10)
+
+entryId = Entry(root, width=25, bd=5, font=('Arial bold', 15))
+entryName = Entry(root, width=25, bd=5, font=('Arial bold', 15))
+entryPrice = Entry(root, width=25, bd=5, font=('Arial bold', 15))
+entryQuantity = Entry(root, width=25, bd=5, font=('Arial bold', 15))
+entryId.grid(row=1, column=1, columnspan=3, padx=5, pady=5)
+entryName.grid(row=2, column=1, columnspan=3, padx=5, pady=5)
+entryPrice.grid(row=3, column=1, columnspan=3, padx=5, pady=5)
+entryQuantity.grid(row=4, column=1, columnspan=3, padx=5, pady=5)
+
+buttonEnter = Button(
+    root, text="Enter", padx=5, pady=5, width=5,
+    bd=3, font=('Arial', 15), bg="#0099ff", command=insert_data)
+buttonEnter.grid(row=5, column=1, columnspan=1)
+
+buttonUpdate = Button(
+    root, text="Update", padx=5, pady=5, width=5,
+    bd=3, font=('Arial', 15), bg="#ffff00", command=update_data)
+buttonUpdate.grid(row=5, column=2, columnspan=1)
+
+buttonDelete = Button(
+    root, text="Delete", padx=5, pady=5, width=5,
+    bd=3, font=('Arial', 15), bg="#e62e00", command=delete_data)
+buttonDelete.grid(row=5, column=3, columnspan=1)
+
+buttonImport = Button(
+    root, text="Import", padx=5, pady=5, width=5,
+    bd=3, font=('Arial', 15), bg="#80D0C7", command=import_data)
+buttonImport.grid(row=5, column=4, columnspan=1)
+
+style = ttk.Style()
+style.configure("Treeview.Heading", font=('Arial bold', 15))
+
+my_tree['columns'] = ("ID", "Name", "Price", "Quantity")
+my_tree.column("#0", width=0, stretch=NO)
+my_tree.column("ID", anchor=W, width=100)
+my_tree.column("Name", anchor=W, width=200)
+my_tree.column("Price", anchor=W, width=150)
+my_tree.column("Quantity", anchor=W, width=150)
+my_tree.heading("ID", text="ID", anchor=W)
+my_tree.heading("Name", text="Name", anchor=W)
+my_tree.heading("Price", text="Price", anchor=W)
+my_tree.heading("Quantity", text="Quantity", anchor=W)
+
+for data in my_tree.get_children():
+    my_tree.delete(data)
+
+for result in reverse(read()):
+    my_tree.insert(parent='', index='end', iid=0,
+                   text="", values=(result), tag="orow")
+
+my_tree.tag_configure('orow', background='#EEEEEE', font=('Arial bold', 15))
+my_tree.grid(row=1, column=5, columnspan=4, rowspan=5, padx=10, pady=10)
+
+root.mainloop()
